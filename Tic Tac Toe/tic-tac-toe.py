71700ff98d5ed9fa745e6b2002e0a386e812a1b5
Ertuğrul Sarıhan <74004586+ertugrulsarihan@users.noreply.github.com>
2021-02-06 17:38:19 +0300

    Update tic-tac-toe.py

--- Tic Tac Toe/tic-tac-toe.py
+++ Tic Tac Toe/tic-tac-toe.py
@@ -1,4 +1,4 @@
-# Implementation of Two Player Tic-Tac-Toe game in Python.
+
 
 
 theBoard = {'1': ' ', '2': ' ', '3': ' ',
@@ -10,10 +10,6 @@ boardKeys = []
 for key in theBoard:
     boardKeys.append(key)
 
-''' We will have to print the updated board after every move in the game and 
-    thus we will make a function in which we'll define the printBoard function
-    so that we can easily print the board everytime by calling this function. '''
-
 
 def printBoard(board):
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
@@ -23,7 +19,7 @@ def printBoard(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
 
 
-# Now we'll write the main function which has all the gameplay functionality.
+
 def game():
     turn = 'X'
     count = 0
@@ -41,7 +37,7 @@ def game():
             print("Burası zaten dolu.\nNereyi dolduracaksın?")
             continue
 
-        # Now we will check if player X or O has won,for every move after 5 moves.
+       
         if count >= 5:
             if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ':  # across the top
                 printBoard(theBoard)
@@ -84,18 +80,17 @@ def game():
                 print(" **** " + turn + " won. ****")
                 break
 
-                # If neither X nor O wins and the board is full, we'll declare the result as 'tie'.
         if count == 9:
             print("\nKaybettin.\n")
             print("It's a Tie!!")
 
-        # Now we have to change the player after every move.
+       
         if turn == 'X':
             turn = 'O'
         else:
             turn = 'X'
 
-            # Now we will ask if player wants to restart the game or not.
+          
     restart = input("Tekrar oynamak istersmisin?(y/n)")
     if restart == "y" or restart == "Y":
         for key in boardKeys:
